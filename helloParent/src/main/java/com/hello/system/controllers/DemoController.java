67aea796c8ae5f36ef8e3ea4cb3104cb8ff8e9e3
vastStars <zili.wang.2017@gmail.com>
2017-05-25 16:32:51 +0800

    搭建基本springMvc框架

--- helloParent/src/main/java/com/hello/system/controllers/DemoController.java
+++ helloParent/src/main/java/com/hello/system/controllers/DemoController.java
@@ -1,5 +1,7 @@
 package com.hello.system.controllers;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,9 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
  **/
 @Controller
 public class DemoController {
+
+    @Autowired
+    private BeanFactory beanFactory;
+
     @RequestMapping("/index")
     public String index(){
         System.out.println("--------------000----------");
+        Object  m = beanFactory.getBean("iWantKnow");
+        System.out.println(m.toString());
         return "login";
     }
 }
